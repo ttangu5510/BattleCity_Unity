546e6b73b4cb3cc532b78c0725d6b307b96354ad
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-04-29 16:28:55 +0900

    [Feat] Add Bullet ObjectPool component
    - 포탄 프리팹, 포탄 오브젝트 풀 구현 완료
    - Tank_Player 프리팹, PlayerController  스크립트 세부 수정

--- Assets/00_JYL_Bullet/Scripts/BulletObjectPool.cs
+++ Assets/00_JYL_Bullet/Scripts/BulletObjectPool.cs
@@ -1,19 +1,41 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 public class BulletObjectPool : MonoBehaviour
 {
     private Stack<PooledObject> bulletStack;
-    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] PooledObject bulletPrefab;
+    [SerializeField] GameObject bulletOutEffect;
     [SerializeField] int poolSize;
     private void Awake()
     {
         bulletStack = new Stack<PooledObject>();
         for (int i = 0; i < poolSize; i++)
         {
-            //PooledObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            PooledObject bullet = Instantiate(bulletPrefab, transform);
+
+            bullet.gameObject.SetActive(false);
+            bulletStack.Push(bullet);
         }
+
+
+    }
+
+    public PooledObject BulletOut()
+    {
+        PooledObject bulletOut = bulletStack.Pop();
+        bulletOut.returnPool = this;
+        bulletOut.gameObject.SetActive(true);
+        bulletOut.GetComponent<Rigidbody>();
+
+        return bulletOut;
+    }
+
+    public void ReturnToPool(PooledObject bullet)
+    {
+        Instantiate(bulletOutEffect,transform.position, transform.rotation,transform);
+        bullet.transform.rotation = Quaternion.identity;
+        bullet.gameObject.SetActive(false);
+        bulletStack.Push(bullet);
     }
 }
--- Assets/00_JYL_Bullet/Scripts/PooledObject.cs
+++ Assets/00_JYL_Bullet/Scripts/PooledObject.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class PooledObject : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public BulletObjectPool returnPool;
+    
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        returnPool.ReturnToPool(this);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
