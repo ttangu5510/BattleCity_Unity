d439c158123c8202dddd37040d7f25ad6e39339f
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-04-29 10:51:16 +0900

    [Feat] Add BulletObjectPool
    - 오브젝트 풀 코딩 중

--- Assets/00_JYL_Bullet/Scripts/BulletObjectPool.cs
+++ Assets/00_JYL_Bullet/Scripts/BulletObjectPool.cs
@@ -6,6 +6,14 @@ using UnityEngine.Events;
 public class BulletObjectPool : MonoBehaviour
 {
     private Stack<PooledObject> bulletStack;
+    [SerializeField] GameObject bulletPrefab;
     [SerializeField] int poolSize;
-
+    private void Awake()
+    {
+        bulletStack = new Stack<PooledObject>();
+        for (int i = 0; i < poolSize; i++)
+        {
+            //PooledObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        }
+    }
 }
