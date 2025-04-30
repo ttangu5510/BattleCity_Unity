ebaf7ee678110df332f0068a7b36fd07ebf1f139
CGRJ2 <80324256+CGRJ2@users.noreply.github.com>
2025-05-01 00:35:06 +0900

    [Fix] Fixed Problem [Enemy Damagable Can not Reached Cause of Spawner]
    - 포탄 충돌처리 내 IDamagable 처리 방식
    IDamagable damagable = ollision.gameObject.transform.root.GetComponent<IDamagable>(); << 이 방법으로 변경 이후 Spawner 자식에 존재하던 Enemy의 콜라이더가 최상위 부모 오브젝트로 Spawner를 반환하며 Enemy의 IDamagable에 접근하지 못하던 문제 해결
    
    - 해결 방법 : EnemySpawner 에 IDamagable 인터페이스 구현 후, TakeDamage함수에서 충돌 받은 Enemy(IDamagable)의 TakeDamage함수 실행

--- Assets/01_CJM_EnemyAndStageManager/Scripts/Enemy/EnemySpawner.cs
+++ Assets/01_CJM_EnemyAndStageManager/Scripts/Enemy/EnemySpawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySpawner : MonoBehaviour
+public class EnemySpawner : MonoBehaviour, IDamagable
 {
     [SerializeField] private SpawnerState state;
     [SerializeField] private Transform standByGroup;
@@ -31,11 +31,14 @@ public class EnemySpawner : MonoBehaviour
         // 자식 활성화
         if (!standByGroup.GetChild(0).gameObject.activeSelf)
             standByGroup.GetChild(0).gameObject.SetActive(true);
+    }
 
-
-        // 수정해보자.
-
-
+    public void TakeDamage() 
+    {
+        // 어차피 콜라이더 없어서 직접적인 충돌은 없고
+        // 자식 오브젝트로 들어갈 Enemy들의 Damagable을 전달하는 방식
+        IDamagable damagable = standByGroup.GetChild(0).gameObject.GetComponent<Enemy>();
+        damagable?.TakeDamage();
     }
 
     // TODO:
