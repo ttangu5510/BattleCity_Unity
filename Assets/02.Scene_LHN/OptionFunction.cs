008547223fe07fbc772285fd6703654b4cebd1d5
이하나 <45592710+leehana0921@users.noreply.github.com>
2025-04-30 18:46:38 +0900

    [Feat] 해상도 변경 기능 추가

--- Assets/02.Scene_LHN/OptionFunction.cs
+++ Assets/02.Scene_LHN/OptionFunction.cs
@@ -1,25 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
+using static UnityEditor.Progress;
 
 public class OptionFunction : MonoBehaviour
 {
 
-    public Dropdown resolutionsDropdown;
+    public TMP_Dropdown resolutionsDropdown;
     List<Resolution> resolutions = new List<Resolution>();
+    public int resolutionNum;
 
-    void Update()
+    void Start()
     {
-        resolutions.AddRange(Screen.resolutions);
+        for (int i = 0; i < Screen.resolutions.Length; i++)
+        {
+            //if (Screen.resolutions[i].refreshRateRatio == 60)
+            // 여기서 지금 오류가 나고 있음 나중에 여쭤보기
+            
+                resolutions.Add(Screen.resolutions[i]);
+        }
         resolutionsDropdown.options.Clear();
 
+        int optionNum = 0;
         foreach (Resolution item in resolutions)
         {
-            Dropdown.OptionData option = new Dropdown.OptionData();
+            TMP_Dropdown.OptionData option = new TMP_Dropdown.OptionData();
             option.text = item.width + "x" + item.height + " " + item.refreshRateRatio + "hz";
             resolutionsDropdown.options.Add(option);
+
+            if(item.width == Screen.width && item.height == Screen.height)
+                resolutionsDropdown.value = optionNum;
+            optionNum++;
+
+          
         }
+        TMP_Dropdown.OptionData option1 = new TMP_Dropdown.OptionData();
+        option1.text = " ";
+        resolutionsDropdown.options.Add(option1);
+
         resolutionsDropdown.RefreshShownValue();
+    }
+
+    public void DropboxOptionChange(int x)
+    {
+        resolutionNum = x;
     }
 }
