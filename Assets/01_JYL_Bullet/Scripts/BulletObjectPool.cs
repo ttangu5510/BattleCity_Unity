b824a3cae8f1db4ec357b3b47655f759d19de97c
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-04-30 17:41:54 +0900

    [Feat] Make Bullet can explode walls
    - 포탄이 벽에 충돌 시, 범위만큼 파괴하는 로직을 구현. Physics.OverlapBox를 통해 구현

--- Assets/01_JYL_Bullet/Scripts/BulletObjectPool.cs
+++ Assets/01_JYL_Bullet/Scripts/BulletObjectPool.cs
@@ -13,12 +13,9 @@ public class BulletObjectPool : MonoBehaviour
         for (int i = 0; i < poolSize; i++)
         {
             PooledObject bullet = Instantiate(bulletPrefab, transform);
-            // TODO : 발사자 정보 추가
             bullet.gameObject.SetActive(false);
             bulletStack.Push(bullet);
         }
-
-
     }
 
     public PooledObject BulletOut()
--- Assets/01_JYL_Bullet/Scripts/PooledObject.cs
+++ Assets/01_JYL_Bullet/Scripts/PooledObject.cs
@@ -1,21 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.ParticleSystemJobs;
 
 public class PooledObject : MonoBehaviour
 {
     public BulletObjectPool returnPool;
-    // TODO : 발사자 정보 추가
-    
+    public enum BulletType { Type1, Type2 };
     [SerializeField] ParticleSystem bulletExplosion;
     private Rigidbody rigid;
+    public BulletType bulletType;
+    private Vector3 range;
+    [SerializeField] private LayerMask Breakable;
     private void Awake()
     {
         rigid ??= GetComponent<Rigidbody>();
+        range = new Vector3(5, 10, 1);
+        Breakable = LayerMask.GetMask("Brick", "SolidBlock");
     }
     private void Update()
-    { 
+    {
         if (rigid.velocity.magnitude > 3)
         {
             //포탄이 곡선을 그리며 날아간다
@@ -28,16 +29,38 @@ public class PooledObject : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("충돌 시작");
         Instantiate(bulletExplosion, transform.position, transform.rotation).Play();
         // TODO : 레이어에 따라서 다른 결과 연산
-        
+        // 비트시프트를 사용해서 찾는 방법과, NameToLayer를 통해서 찾을 수 있다
+        // layer는 LayerMask와 같지 않다. 이것을 이해해야 해결되는 문제
+        // if((1 << collision.gameObject.layer & Breakable.value) == 1 << collision.gameObject.layer)
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Brick") || collision.gameObject.layer == LayerMask.NameToLayer("SolidBlock"))
+        {
+            Debug.Log($"{collision.gameObject.name}벽돌에 부딪힘");
+            Collider[] colliders = Physics.OverlapBox(transform.position + transform.forward, range, Quaternion.identity, Breakable);
+            foreach (var collide in colliders)
+            {
+                collide.gameObject.SetActive(false);
+                Debug.Log($"{collide.gameObject.name}");
+            }
+        }
+        else if (collision.gameObject.layer == Breakable && bulletType == BulletType.Type2)
+        {
+
+        }
+
         IDamagable damagable = GetComponent<IDamagable>();
         if (damagable != null)
         {
             damagable.TakeDamage();
         }
 
-
         returnPool.ReturnToPool(this);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawCube(transform.position + transform.forward, range);
     }
 }
