595ff78fe8f5d191fc24051ab280dd9137d85955
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-05-01 23:41:04 +0900

    [Feat] Add GameManager Script
    - 게임매니저 스크립트 작성
    - 싱글톤 구현 완료

--- Assets/01_JYL_Bullet/Scripts/GameManager.cs
+++ Assets/01_JYL_Bullet/Scripts/GameManager.cs
@@ -1,18 +1,56 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    private GameManager instance;
+    public GameManager Instance
         
+    #region 싱글톤 설정
+
+    {
+        get
+        {
+            if (instance == null)
+            {
+                GameObject go = new GameObject("GameManager");
+                instance = go.AddComponent<GameManager>();
+            }
+            return instance;
+        }
+    }
+    private void Awake()
+    {
+        CreateGameManager();
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameManager CreateGameManager()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        return instance;
+    }
+    #endregion
+    // 싱글톤 필드
+    public int topScore;
+
+    // 싱글톤 함수
+    public void StageComplete()
+    {
+        // 스테이지 클리어 시 수행
+    }
+    public void GameComplete()
+    {
+        // 마지막 스테이지 클리어 시 수행
+    }
+    public void GameOver()
+    {
+        // 게임 오버 시 수행
     }
 }
