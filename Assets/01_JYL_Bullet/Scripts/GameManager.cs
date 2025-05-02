e67caefde3aff2624fe3a57aaed1c485e39267cc
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-05-02 13:33:18 +0900

    [Feat] Update GameManager script
    - 게임 매니저 스크립트 내용들 채우는 중

--- Assets/01_JYL_Bullet/Scripts/GameManager.cs
+++ Assets/01_JYL_Bullet/Scripts/GameManager.cs
@@ -1,12 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
+public struct ScoreBoard
+{
+    public string name;
+    public int score;
+}
 public class GameManager : MonoBehaviour
 {
     private GameManager instance;
-    public GameManager Instance
-        
+    public GameManager Instance 
     #region 싱글톤 설정
-
     {
         get
         {
@@ -21,6 +28,17 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         CreateGameManager();
+        lastStageNum = 25;
+        scores = new ScoreBoard[10];
+        for(int i = 2; i <= lastStageNum;i++)
+        {
+            stageSceneName.Enqueue($"STAGE {i}");
+        }
+        for(int i =0;i<scores.Length;i++)
+        {
+            scores[i].name = "BattleCity";
+            scores[i].score = 10+10*i;
+        }
     }
 
     public GameManager CreateGameManager()
@@ -37,20 +55,80 @@ public class GameManager : MonoBehaviour
         return instance;
     }
     #endregion
-    // 싱글톤 필드
-    public int topScore;
 
+    // 싱글톤 필드
+    private int lastStageNum;
+    private Coroutine waitRoutine;
+    private YieldInstruction waitSec;
+    public ScoreBoard[] scores;
+    // 씬 큐
+    private Queue<string> stageSceneName; 
     // 싱글톤 함수
     public void StageComplete()
     {
-        // 스테이지 클리어 시 수행
+        // 씬 전환
+        // 1. 점수 합산 창 씬 -> 2. 로딩창 함수 -> 3. 다음 스테이지 씬
+        // 1. 점수 합산 창 씬 불러오기
+        SceneManager.LoadSceneAsync("점수 합산 창 씬");
+        // 1-1 로딩 결과 좀 보다가 넘어가기
+        WaitForSeconds(3f);
+        // 2. 로딩창 자동 - 씬의 이름을 넣어야함(해결)
+        // 3. 다음 스테이지 씬 불러오기
+        // 기존 씬 기준으로 다음 씬 판단이 필요함
+        // 씬 큐가 필요 -> stageSceneName
+        MySceneManager.Instance.ChangeScene(stageSceneName.Dequeue());
+        // 플레이어 정보를 기준으로 다음 스테이지 씬에서 리스폰 포인트에 생성(씬 자체에 기능적으로 구현 되어 있음)
     }
+
     public void GameComplete()
     {
         // 마지막 스테이지 클리어 시 수행
+        // 씬 전환 -> 1. 점수 합산 창 씬 -> 2. 게임 클리어(Congratulations) 씬 -> 3. 점수 씬 입력 (뉴레코드 일 때) -> 4. 레코드 씬 -> 5. 타이틀 씬
+        // 초기화 작업
+        // 스테이지 큐 다시 채우기
+
+        // 1. 점수 합산 창
+        SceneManager.LoadSceneAsync("점수 합산 창 씬");
+        WaitForSeconds(3f);
+        // 2. 게임 클리어 씬
+        SceneManager.LoadSceneAsync("게임 클리어 씬");
+        // 3. 점수 입력 씬 (뉴레코드 일 때)
+        SceneManager.LoadSceneAsync("점수 입력 씬");
+        // 4. 레코드 씬
+        SceneManager.LoadSceneAsync("레코드 씬");
+        WaitForSeconds(3f);
     }
+
     public void GameOver()
     {
         // 게임 오버 시 수행
+        // 게임 오버 UI -> 점수 합산 창 씬 -> 게임 오버 씬 -> 점수 씬(뉴레코드 일 때) -> 타이틀 씬
+        // 1. 게임 오버 UI
+        // 2. 점수 합산 창 씬
+        // 3. 게임 오버 씬
+        // 4. 점수 씬(뉴 레코드 일 때)
+        // 5. 타이틀 씬
+    }
+
+    // Countinue
+
+
+    private void WaitForSeconds(float sec)
+    {
+        waitSec = new WaitForSeconds(sec);
+        if(waitRoutine == null)
+        {
+            waitRoutine = StartCoroutine(WaitSecondsRoutine());
+        }
+        else
+        {
+            StopCoroutine(waitRoutine);
+            waitRoutine = null;
+            waitRoutine = StartCoroutine(WaitSecondsRoutine());
+        }
+    }
+    IEnumerator WaitSecondsRoutine()
+    {
+        yield return waitSec;
     }
 }
