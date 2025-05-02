3443f8ae8e53125dfe42edba3cb27e53df2e1b0b
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-05-02 17:02:21 +0900

    [Test] Add Testcass of GameManager methods
    - StageComplete, GameComplete를 위한 테스트 케이스
    - 씬 전환이 제대로 원하는 대로 되는지 확인하기 위해 테스트 씬들 제작

--- Assets/01_JYL_Bullet/Scripts/GameManager.cs
+++ Assets/01_JYL_Bullet/Scripts/GameManager.cs
@@ -3,22 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using TMPro;
 
+// 게임 기록을 담을 구조체
 public struct ScoreBoard
 {
     public string name;
     public int score;
 
 }
+
 public class GameManager : MonoBehaviour
 {
-    private GameManager instance;
-    public GameManager Instance 
+    private static GameManager instance;
+    public static GameManager Instance
     #region 싱글톤 설정
     {
         get
         {
+            // Lazy Initialization
             if (instance == null)
             {
                 GameObject go = new GameObject("GameManager");
@@ -30,23 +32,27 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         CreateGameManager();
-        lastStageNum = 25;
+        lastStageNum = 2;
+        waitSec = new WaitForSeconds(2f);
+        stageSceneName = new Queue<string>();
+        // TODO: Test GameManager
         scores = new ScoreBoard[10];
-        for(int i = 2; i <= lastStageNum;i++)
+        // TODO :Test GameManager
+        for (int i = 1; i <= 2; i++)
         {
-            //TODO: 스테이지 만들고 추가
-            //stageSceneName.Enqueue($"STAGE {i}");
+            stageSceneName.Enqueue($"JYL_STAGE {i}");
         }
-        for(int i =0;i<scores.Length;i++)
+        //TODO: 스테이지 만들고 추가
+        //for(int i = 2; i <= lastStageNum;i++)
+        //{
+        //    //stageSceneName.Enqueue($"STAGE {i}");
+
+        //}
+        for (int i = 0; i < scores.Length; i++)
         {
-            scores[i].name = $"BattleCity{i+1}";
-            scores[i].score = 500*(i+1);
+            scores[i].name = "BattleCity";
+            scores[i].score = 500 * (i * i);
         }
-    }
-    // TODO : Test SortScore
-    private void Start()
-    {
-        SortScore(scores);
     }
     public GameManager CreateGameManager()
     {
@@ -69,25 +75,35 @@ public class GameManager : MonoBehaviour
     private YieldInstruction waitSec;
     public ScoreBoard[] scores;
     // 씬 큐
-    private Queue<string> stageSceneName; 
+    private Queue<string> stageSceneName;
     // 싱글톤 함수
     public void StageComplete()
+    {
+        StartCoroutine(StageCompleteRoutine());
+    }
+    IEnumerator StageCompleteRoutine()
     {
         // 씬 전환
         // 1. 점수 합산 창 씬 -> 2. 로딩창 함수 -> 3. 다음 스테이지 씬
         // 1. 점수 합산 창 씬 불러오기
-        SceneManager.LoadSceneAsync("점수 합산 창 씬");
+        SceneManager.LoadSceneAsync("JYL_StageResultScene");
         // 1-1 로딩 결과 좀 보다가 넘어가기
-        WaitForSeconds(3f);
+        yield return waitSec;
         // 2. 로딩창 자동 - 씬의 이름을 넣어야함(해결)
         // 3. 다음 스테이지 씬 불러오기
         // 기존 씬 기준으로 다음 씬 판단이 필요함
         // 씬 큐가 필요 -> stageSceneName
+        // TODO: Test ChangeScene by GameManager
+        //SceneManager.LoadSceneAsync(stageSceneName.Dequeue());
         MySceneManager.Instance.ChangeScene(stageSceneName.Dequeue());
         // 플레이어 정보를 기준으로 다음 스테이지 씬에서 리스폰 포인트에 생성(씬 자체에 기능적으로 구현 되어 있음)
     }
-
     public void GameComplete()
+    {
+        StartCoroutine(GameCompleteRoutine());
+
+    }
+    IEnumerator GameCompleteRoutine()
     {
         // 마지막 스테이지 클리어 시 수행
         // 씬 전환 -> 1. 점수 합산 창 씬 -> 2. 게임 클리어(Congratulations) 씬 -> 3. 점수 씬 입력 (뉴레코드 일 때) -> 4. 레코드 씬 -> 5. 타이틀 씬
@@ -95,16 +111,37 @@ public class GameManager : MonoBehaviour
         // 스테이지 큐 다시 채우기
 
         // 1. 점수 합산 창
-        SceneManager.LoadSceneAsync("점수 합산 창 씬");
-        WaitForSeconds(3f);
+        SceneManager.LoadSceneAsync("JYL_StageResultScene");
+        yield return waitSec;
         // 2. 게임 클리어 씬
-        SceneManager.LoadSceneAsync("게임 클리어 씬");
+        SceneManager.LoadSceneAsync("JYL_GameClearScene");
+        yield return waitSec;
         // 3. 점수 입력 씬 (뉴레코드 일 때)
-        SceneManager.LoadSceneAsync("점수 입력 씬");
+        // 이름과 점수를 게임매니저에 저장함
+        // while로 이름 입력이 끝나거나, 카운트다운 코루틴이 끝났을 시 다음으로 진행
+        if (PlayerData.Instance.score >= scores[9].score)
+        {
+            InputNewScore();
+        }
+        yield return waitSec;
         // 4. 레코드 씬
-        SceneManager.LoadSceneAsync("레코드 씬");
-        WaitForSeconds(3f);
+        // 이름과 점수를 1위부터 10위까지 나열해서 표시하는 씬
+        SceneManager.LoadSceneAsync("JYL_RecordScene");
+        yield return waitSec;
+        SceneManager.LoadSceneAsync("TitleScene");
     }
+
+    // 점수 합산 씬
+    // 필요 요소
+
+    // UI  각 적 모양마다 이미지 X 점수 = 합점수
+    // 각 적 모양마다 이미지= StageManager.Instance.GetSlayeeEnemyCountByGrade(EnemyGrade grade) == 적 등급(grade)을 한 스테이지에서 잡은 숫자(int)
+    // EnemyManager 싱글톤 스크립트 == 등급마다 점수, 아예 등급 X 점수 = 합점수 함수가 있음
+    // 몬스터 갯수만큼
+    // -----------
+    // 최종점수
+
+
 
     public void GameOver()
     {
@@ -116,34 +153,12 @@ public class GameManager : MonoBehaviour
         // 4. 점수 씬(뉴 레코드 일 때)
         // 5. 타이틀 씬
     }
-
-    // Countinue
-
-
-    private void WaitForSeconds(float sec)
-    {
-        waitSec = new WaitForSeconds(sec);
-        if(waitRoutine == null)
-        {
-            waitRoutine = StartCoroutine(WaitSecondsRoutine());
-        }
-        else
-        {
-            StopCoroutine(waitRoutine);
-            waitRoutine = null;
-            waitRoutine = StartCoroutine(WaitSecondsRoutine());
-        }
-    }
-    IEnumerator WaitSecondsRoutine()
-    {
-        yield return waitSec;
-    }
     public void SortScore(ScoreBoard[] scores)
     {
-        Array.Sort(scores,(scoreA,scoreB)=>scoreB.score.CompareTo(scoreA.score));
-        for (int i = 0; i < scores.Length; i++)
-        {
-            Debug.Log($"{i+1}위 {scores[i].name}  점수: {scores[i].score}");
-        }
+        Array.Sort(scores, (scoreA, scoreB) => scoreB.score.CompareTo(scoreA.score));
+    }
+    public void InputNewScore()
+    {
+        SceneManager.LoadSceneAsync("JYL_InputRecordScene");
     }
 }
---
+++ Assets/01_JYL_Bullet/Scripts/Test_GameManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Test_GameManager : MonoBehaviour
+{
+ 
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            // 스테이지 컴플리트 테스트
+            GameManager.Instance.StageComplete();
+        }
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            // 게임 클리어 테스트
+            GameManager.Instance.GameComplete();
+        }
+    }
+
+}
+
