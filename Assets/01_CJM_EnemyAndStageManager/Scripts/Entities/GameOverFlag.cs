01fbcebcdae1638375fe5b92769b85d049521284
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-05-05 14:57:36 +0900

    [Style] Cleanup prefabs
    - 프리팹 들을 폴더들로 분류

--- Assets/01_CJM_EnemyAndStageManager/Scripts/Entities/GameOverFlag.cs
+++ Assets/01_CJM_EnemyAndStageManager/Scripts/Entities/GameOverFlag.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class GameOverFlag : MonoBehaviour
 {
+    [SerializeField] GameObject BrokenObject;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Bullet")
         {
+            Instantiate(BrokenObject,transform.position,transform.rotation);
+            Destroy(gameObject);
             StageManager.Instance.StageFail();
         }
     }
