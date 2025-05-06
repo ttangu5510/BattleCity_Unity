9bade83bd5ef3bca63e95f5eefc16270b963966b
hegon910 <4poise@gmail.com>
2025-05-07 00:28:19 +0900

    [Fix] Fixed Frame about PHG_Map2

--- Assets/02_PHG/Scripts/Funtionable/LightFlicker.cs
+++ Assets/02_PHG/Scripts/Funtionable/LightFlicker.cs
@@ -7,14 +7,17 @@ public class LightFlicker : MonoBehaviour
     public AudioClip flickerClip;
     public float minInterval = 0.05f;      // √÷º“ ±Ù∫˝¿” ∞£∞›
     public float maxInterval = 5f;       // √÷¥Î ±Ù∫˝¿” ∞£∞›
-
+    public bool startOn =  true;
     public float offDuration = 0.05f;      // ≤®¡Æ ¿÷¥¬ Ω√∞£
-    private float timer;
+    [SerializeField] public float startDelay = 0f;
 
     void Start()
     {
         if (flickerLight == null)
             flickerLight = GetComponent<Light>();
+
+
+        flickerLight.enabled = startOn;
 
         StartCoroutine(FlickerRoutine());
     }
@@ -29,13 +32,22 @@ public class LightFlicker : MonoBehaviour
 
     private System.Collections.IEnumerator FlickerRoutine()
     {
+        if (startDelay > 0f)
+            yield return new WaitForSeconds(startDelay);
+
+        flickerLight.enabled = !flickerLight.enabled;
+        PlayFlickerSound();
+        yield return new WaitForSeconds(offDuration);
+
+        flickerLight.enabled = !flickerLight.enabled;
+        PlayFlickerSound();
+
         while (true)
         {
             yield return new WaitForSeconds(Random.Range(minInterval, maxInterval));
 
             flickerLight.enabled = false;
             PlayFlickerSound();
-
             yield return new WaitForSeconds(offDuration);
 
             flickerLight.enabled = true;
